359e6d1a62813646abc043ca1a1cf0cd5d33f03b
Michael <michael@nowhere.tld>
2019-02-05 12:45:26 -0600

    White/Black list support. Email alerting support. Elasticsearch logging support and many other enahncements

--- pyfalcon.py
+++ pyfalcon.py
@@ -14,13 +14,30 @@ import ssl
 import threading
 import traceback
 import logging
-
 import sys
 from requests import Request, Session
 
 
+class ES:
+
+    def __init__(self, config, logger):
+        import elasticsearch
+        self.es = elasticsearch.Elasticsearch(hosts=config['hosts'])
+        self.index_name = config['index']
+        self.lh = logger
+        self.doctype = config['doctype']
+
+    def index(self, data, id, ts):
+        try:
+            self.es.create(index=self.index_name,
+                           doc_type=self.doctype, id=id, body=data, timestamp=ts)
+        except Exception as e:
+            self.lh.exception('Elasticsearch index error:' + str(e))
+
+
 class StreamProcessor(threading.Thread):
-    def __init__(self, stream, stream_resource, processor, logger, offsets_file="./offsets_file.json"):
+
+    def __init__(self, stream, stream_resource, processor, logger, offsets_file='./offsets.json', es=None):
         threading.Thread.__init__(self)
         self.stream = stream
         self.stream_resource = stream_resource
@@ -30,71 +47,85 @@ class StreamProcessor(threading.Thread):
         self.offsets = {}
         self.offsets_file = offsets_file
         try:
-            with open(offsets_file, "w+") as of:
+            with open(offsets_file, 'w+') as of:
                 data = of.read()
                 if data and data.strip():
                     offsets = json.loads()
                 else:
-                    self.offsets[self.stream_resource["dataFeedURL"]] = 0
+                    self.offsets[self.stream_resource['dataFeedURL']] = 0
                     of.write(json.dumps(self.offsets))
+            self.es = es
         except:
-            self.lh.exception("offset file error")
+            self.lh.exception('offset file error')
 
     def run(self):
-        self.lh.debug("Started a StreamProcessor thread")
+        self.lh.debug('Started a StreamProcessor thread')
         try:
             while self.running:
                 for stream_data in self.stream.iter_lines():
                     if stream_data.strip():
                         try:
                             json_data = json.loads(stream_data)
-                            offset = json_data["metadata"]["offset"]
-                            if offset > self.offsets[self.stream_resource["dataFeedURL"]]+100:
-                                self.offsets[self.stream_resource["dataFeedURL"]] = offset
-                                with open(self.offsets_file, "w+") as of:
+                            offset = json_data['metadata']['offset']
+                            if offset > self.offsets[self.stream_resource['dataFeedURL']] + 100:
+                                self.offsets[self.stream_resource['dataFeedURL']] = offset
+                                with open(self.offsets_file, 'w+') as of:
                                     of.write(json.dumps(self.offsets))
-
                             self.processor(json_data)
+                            if self.es:
+                                if 'eventCreationTime' in json_data['metadata']:
+                                    json_data['metadata']['eventCreationTime'] = json_data['metadata']['eventCreationTime'] / 1000
+                                    ts = datetime.datetime.utcfromtimestamp(
+                                        json_data['metadata']['eventCreationTime']).strftime('%Y-%m-%dT%H:%M:%S%z')
+                                    json_data['ts'] = ts
+                                    self.es.index(json_data, offset, ts)
                         except ValueError:
-                            self.lh.exception("Can't decode:\n"+stream_data)
+                            self.lh.exception("Can't decode:\n" + stream_data)
                             continue
                         except Exception as e:
                             self.lh.exception(str(e))
                             traceback.print_exc()
                             continue
+
         except Exception as e:
-            self.lh.exception("Stream processor thread exception:"+str(e))
-        self.lh.debug("StreamProcessor exiting for stream:\n" +
-                      self.stream_resource["dataFeedURL"])
+            self.lh.exception('Stream processor thread exception:' + str(e))
+
+        self.lh.debug('StreamProcessor exiting for stream:\n' +
+                      self.stream_resource['dataFeedURL'])
 
 
 class FalconStreamingAPI:
+
     def __init__(self, config, processor, logger=None):
         if not logger:
             self.lh = logging.getLogger('PyFalcon')
             self.lh.setLevel(logging.DEBUG)
             logging.basicConfig(format='PyFalcon: %(asctime)-15s  %(message)s')
-            self.lh.info("Starting Falcon streaming api script...")
+            self.lh.info('Starting Falcon streaming api script...')
         else:
             self.lh = logger
         self.config = config
-        self.key = config["falcon_api_key"]
-        self._id = config["falcon_api_id"]
-        self.Method = "GET"
+        if self.config['Elasticsearch_logging']:
+            self.es = ES(self.config['Elasticsearch'], self.lh)
+        else:
+            self.es = None
+        self.key = config['falcon_api_key']
+        self._id = config['falcon_api_id']
+        self.Method = 'GET'
         self.md5 = ''
-        self.url = config["falcon_data_feed_url"] + \
-            "?appId="+config["client_name"]
-        self.RequestUri_Host = config["falcon_hose_domain"]
+        self.url = config['falcon_data_feed_url'] + \
+            '?appId=' + config['client_name']
+        self.RequestUri_Host = config['falcon_hose_domain']
         self.RequestUri_AbsolutePath = '/sensors/entities/datafeed/v1'
-        self.RequestUri_Query = '?appId='+config["client_name"]
+        self.RequestUri_Query = '?appId=' + config['client_name']
         self.Headers = {}
         self.processor = processor
         self.reconnect = True
         self.sleeptime = 300
-        if "offsets_file" in config:
-            self.offsets_file = config["offsets_file"]
+        if 'offsets_file' in config:
+            self.offsets_file = config['offsets_file']
         else:
-            self.offsets_file = "./offsets.json"
+            self.offsets_file = './offsets_file.json'
 
     def calculateHMAC(self, _key, _requestString):
         digest = hmac.new(str(_key), msg=str(_requestString),
@@ -106,87 +137,82 @@ class FalconStreamingAPI:
 
     def connect(self):
         try:
-            self.date = datetime.datetime.utcnow().strftime("%a, %d %b %Y %X GMT")
+            self.date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %X GMT')
             self.lh.debug(
-                "Connecting to the streaming api with date stamp:"+self.date)
-            requestString = self.Method+"\n"+self.md5+"\n"+self.date+"\n"+self.RequestUri_Host + \
-                self.RequestUri_AbsolutePath+"\n" + \
+                'Connecting to the streaming api with date stamp:' + self.date)
+            requestString = self.Method + '\n' + self.md5 + '\n' + self.date + '\n' + self.RequestUri_Host + \
+                self.RequestUri_AbsolutePath + '\n' + \
                 self.CanonicalQueryString(self.RequestUri_Query)
             signature = self.calculateHMAC(self.key, requestString)
-            self.Headers["X-CS-Date"] = self.date
-            self.Headers["Authorization"] = "cs-hmac " + \
-                self._id + ":" + signature + ":customers"
+            self.Headers['X-CS-Date'] = self.date
+            self.Headers['Authorization'] = 'cs-hmac ' + \
+                self._id + ':' + signature + ':customers'
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             context = ssl.create_default_context()
             c = context.wrap_socket(socket.socket(
-                socket.AF_INET), server_hostname=self.config["falcon_hose_domain"])
-            self.lh.debug("Connecting to Falcon streaming API using TLS.")
-            c.connect((self.config["falcon_hose_domain"], 443))
-            rs = "GET /sensors/entities/datafeed/v1?appId=" + \
-                self.config["client_name"]+" HTTP/1.1\n"
-            rs += "X-CS-Date: "+self.date+"\n"
-            rs += "Authorization: "+self.Headers["Authorization"]+"\n"
-            rs += "Host: "+self.config["falcon_hose_domain"]+"\n"
-            rs += "Connection: Keep-Alive\r\n\r\n"
+                socket.AF_INET), server_hostname=self.config['falcon_hose_domain'])
+            self.lh.debug('Connecting to Falcon streaming API using TLS.')
+            c.connect((self.config['falcon_hose_domain'], 443))
+            rs = 'GET /sensors/entities/datafeed/v1?appId=' + \
+                self.config['client_name'] + ' HTTP/1.1\n'
+            rs += 'X-CS-Date: ' + self.date + '\n'
+            rs += 'Authorization: ' + self.Headers['Authorization'] + '\n'
+            rs += 'Host: ' + self.config['falcon_hose_domain'] + '\n'
+            rs += 'Connection: Keep-Alive\r\n\r\n'
             rs = rs.encode('utf-8')
-
             c.sendall(rs)
             data = c.recv(10000)
             c.close()
-
             body = data.split('\r\n\r\n')[1]
-
             self.data_stream = json.loads(body)
-            if "errors" in self.data_stream:
-                self.lh.debug("Errors in data stream response:\n" +
+            if 'errors' in self.data_stream:
+                self.lh.debug('Errors in data stream response:\n' +
                               json.dumps(self.data_stream, indent=4, sort_keys=True))
                 self.reconnect = True
                 raise
-
-            elif self.data_stream["meta"]["pagination"] and "total" in self.data_stream["meta"]["pagination"] and self.data_stream["meta"]["pagination"]["total"] > 0:
-                if "resources" in self.data_stream:
-                    self.stream_resources = self.data_stream["resources"]
+            elif self.data_stream['meta']['pagination'] and 'total' in self.data_stream['meta']['pagination'] and self.data_stream['meta']['pagination']['total'] > 0:
+                if 'resources' in self.data_stream:
+                    self.stream_resources = self.data_stream['resources']
                     self.lh.info(
-                        "Discovered "+str(len(self.stream_resources))+" stream resources.")
+                        'Discovered ' + str(len(self.stream_resources)) + ' stream resources.')
                     self.reconnect = False
                 else:
                     self.lh.debug(
-                        "No resrouces:\n"+json.dumps(str(body), indent=4, sort_keys=True))
+                        'No resrouces:\n' + json.dumps(str(body), indent=4, sort_keys=True))
                     self.reconnect = False
                     return False
                 until = 300
                 for stream in self.stream_resources:
                     expiration = stream['sessionToken']['expiration']
                     expires = calendar.timegm(time.strptime(
-                        expiration[:len(expiration)-4]+"Z", "%Y-%m-%dT%H:%M:%S.%fZ"))
+                        expiration[:len(expiration) - 4] + 'Z', '%Y-%m-%dT%H:%M:%S.%fZ'))
                     now = time.time()
-                    if expires-now < until:
-                        until = expires-now  # re-discover streams after waiting for the shortest token expiry time
+                    if expires - now < until:
+                        until = expires - now
                         self.expires = expires
 
-                self.lh.debug("Rediscovering streams in:"+str(until))
+                self.lh.debug('Rediscovering streams in:' + str(until))
                 if until > 295:
-                    self.sleeptime = until  # get new token after expiry
+                    self.sleeptime = until
                 else:
-                    # sessions are supposed to be long lived
-                    self.lh.debug("Short token expiry!:"+str(until))
-                    self.sleeptime = 300  # wait 5 min before new token anyways
-
-                self.lh.debug("New Expiration:"+expiration)
-            elif self.data_stream["meta"]["pagination"] and "total" in self.data_stream["meta"]["pagination"] and self.data_stream["meta"]["pagination"]["total"] == 0:
-                self.lh.debug("Discover attempt resulted in 0 resources")
-                self.reconnect = False
-                return False
+                    self.lh.debug('Short token expiry!:' + str(until))
+                    self.sleeptime = 300
+                self.lh.debug('New Expiration:' + expiration)
             else:
-                self.lh.debug("Unknown response:\n"+str(self.data_stream))
+                if self.data_stream['meta']['pagination'] and 'total' in self.data_stream['meta']['pagination'] and self.data_stream['meta']['pagination']['total'] == 0:
+                    self.lh.debug('Discover attempt resulted in 0 resources')
+                    self.reconnect = False
+                    return False
+                self.lh.debug('Unknown response:\n' + str(self.data_stream))
                 raise
-                self.expires = time.time()+60  # no resources, retry after 60secs
+                self.expires = time.time() + 60
                 self.reconnect = False
         except Exception as e:
             traceback.print_exc()
             self.lh.exception(str(e))
             self.reconnect = True
             return False
+
         return True
 
     def streamData(self):
@@ -197,40 +223,42 @@ class FalconStreamingAPI:
                     offsets = json.loads(of.read())
             except:
                 pass
+
             for i in range(len(self.stream_resources)):
-                ds_headers = {"Authorization": "Token " +
-                              self.stream_resources[i]["sessionToken"]["token"], "Accepts": "appication/json"}
-                self.lh.info("Opening stream for data feed:" +
-                             self.stream_resources[i]["dataFeedURL"])
+                ds_headers = {'Authorization': 'Token ' + self.stream_resources[i]['sessionToken']['token'],
+                              'Accepts': 'appication/json'}
+                self.lh.info('Opening stream for data feed:' +
+                             self.stream_resources[i]['dataFeedURL'])
                 offset = 0
-                if offsets and self.stream_resources[i]["dataFeedURL"] in offsets:
-                    offset = offsets[self.stream_resources[i]["dataFeedURL"]]
-                request_url = self.stream_resources[i]["dataFeedURL"] + \
-                    "&offset="+str(offset)
-                self.lh.debug("DS request URL:"+request_url)
+                if offsets and self.stream_resources[i]['dataFeedURL'] in offsets:
+                    offset = offsets[self.stream_resources[i]['dataFeedURL']]
+                request_url = self.stream_resources[i]['dataFeedURL'] + \
+                    '&offset=' + str(offset)
+                self.lh.debug('DS request URL:' + request_url)
                 response = requests.get(
                     request_url, headers=ds_headers, stream=True)
                 if response.status_code == 200:
                     stream_processor = StreamProcessor(
-                        response, self.stream_resources[i], self.processor, self.lh, offsets_file=self.offsets_file)
+                        response, self.stream_resources[i], self.processor, self.lh, offsets_file=self.offsets_file, es=self.es)
                     stream_processor.setDaemon(True)
                     stream_processor.start()
                     time.sleep(3)
                     if stream_processor.isAlive():
-                        self.lh.debug("Started a new  stream processor thread")
+                        self.lh.debug('Started a new  stream processor thread')
                     else:
-                        self.lh.debug("Stream processor thread is not alive.")
+                        self.lh.debug('Stream processor thread is not alive.')
                 else:
                     self.lh.error(
-                        "Error opening stream '"+self.stream_resources[i]["dataFeedURL"]+"':\n"+response.text)
+                        "Error opening stream '" + self.stream_resources[i]['dataFeedURL'] + "':\n" + response.text)
                     continue
+
             self.stream_resources = []
         except Exception as e:
             self.lh.exception(str(e))
 
 
 def processor(stream_data):
-    print(stream_data)
+    print stream_data
 
 
 def main():
@@ -241,11 +269,9 @@ def main():
     while True:
         try:
             sleeptime = 300
-
             if r.connect():
                 r.streamData()
-                print("sleeping for "+str(sleeptime)+" seconds.")
-
+                print 'sleeping for ' + str(sleeptime) + ' seconds.'
                 time.sleep(sleeptime)
             if not r.reconnect:
                 time.sleep(sleeptime)
@@ -255,8 +281,9 @@ def main():
             continue
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     reload(sys)
-    sys.setdefaultencoding("utf-8")
+    sys.setdefaultencoding('utf-8')
     main()
+
 
--- thehive_falcon.py
+++ thehive_falcon.py
@@ -8,8 +8,24 @@ import json
 import logging
 import requests
 import traceback
+import re
 
 import pyfalcon
+
+
+class ES:
+    def __init__(self, config, logger):
+        import elasticsearch
+        self.es = elasticsearch.Elasticsearch(hosts=config["hosts"])
+        self.index_name = config["index"]
+        self.lh = logger
+        self.doctype = config["doctype"]
+
+    def index(self, data):
+        try:
+            self.es.index(index=self.index_name, doc_type=self.doctype, id=id)
+        except Exception as e:
+            self.lh.exception("Elasticsearch index error:"+str(e))
 
 
 class TheHiveProcessor:
@@ -17,16 +33,164 @@ class TheHiveProcessor:
         self.lh = logh
         self.conf = config
         self.ts = int(time.time())-3600
+        self.whitelist = []
+        self.blacklist = []
+        if self.conf["email_alerts"]:
+            from smtplib import SMTP
+            from email.mime.text import MIMEText
+            self.smtp = SMTP
+            self.mimetext = MIMEText
+
+        self.loadwhitelist(self.conf["whitelist_config"])
+        self.loadblacklist(self.conf["blacklist_config"])
+
+    def email_alert(self, title, event):
+        if not self.conf["email_alerts"]:
+            return
+        notify = self.conf["email_notify"]
+        server = self.conf["email_server"]
+        emailmsg = "<h3>"+title+"</h3><br><pre>" + \
+            json.dumps(event, indent=4, sort_keys=True)+"</pre>"
+        try:
+            email = self.mimetext(emailmsg, 'html')
+            email['Subject'] = title
+            S = self.smtp(server)
+            for recipient in notify:
+                S.sendmail(self.conf["email_from"],
+                           recipient, email.as_string())
+                self.lh.info("Sent email notification to: "+recipient)
+        except Exception as e:
+            self.lh.exception("Email notification exception:"+str(e))
+
+    def loadwhitelist(self, wlconfig):
+        wljson = {}
+        with open(wlconfig) as f:
+            wljson = json.loads(f.read())
+        if "whitelist" in wljson:
+            self.whitelist = wljson["whitelist"]
+            for i in range(0, len(self.whitelist)):
+                #self.lh.debug("Processing whitelist item:"+self.whitelist[i]["name"])
+                if not "selectors" in self.whitelist[i]:
+                    self.whitelist[i]["selector"] = {}
+                else:
+                    selectors = self.whitelist[i]["selectors"]
+                    for selector in selectors:
+                        for j in range(len(selectors[selector])):
+                            if "regex" == selectors[selector][j]["type"]:
+                                # re.compile(selectors[selector][j]["value"])
+                                self.whitelist[i]["selectors"][selector][j]["value"] = selectors[selector][j]["value"]
+                            elif "file" == selectors[selector][j]["type"]:
+                                items = set()
+                                with open(selectors[selector][j]["value"]) as f:
+                                    for line in f.read().splitlines():
+                                        if not line.strip().startswith("#"):
+                                            items.add(line.strip().lower())
+                                self.whitelist[i]["selectors"][selector][j]["value"] = list(
+                                    items)
+
+    def loadblacklist(self, blconfig):
+        bljson = {}
+        with open(blconfig) as f:
+            bljson = json.loads(f.read())
+        if "blacklist" in bljson:
+            self.blacklist = bljson["blacklist"]
+            for i in range(0, len(self.blacklist)):
+                #self.lh.debug("Processing blacklist item:"+self.blacklist[i]["name"])
+                if not "selectors" in self.blacklist[i]:
+                    self.blacklist[i]["selector"] = {}
+                else:
+                    selectors = self.blacklist[i]["selectors"]
+                    for selector in selectors:
+                        for j in range(len(selectors[selector])):
+                            if "regex" == selectors[selector][j]["type"]:
+                                # re.compile(selectors[selector][j]["value"])
+                                self.blacklist[i]["selectors"][selector][j]["value"] = selectors[selector][j]["value"]
+                            elif "file" == selectors[selector][j]["type"]:
+                                items = set()
+                                with open(selectors[selector][j]["value"]) as f:
+                                    for line in f.read().splitlines():
+                                        if not line.strip().startswith("#"):
+                                            items.add(line.strip().lower())
+                                self.blacklist[i]["selectors"][selector][j]["value"] = list(
+                                    items)
+
+    def selectormatch(self, selector_entry, eventval):
+        for item in selector_entry:
+            matched = False
+            if item["type"] == "file":
+                for entry in item["value"]:
+                    if entry == eventval:
+                        matched = True
+                        break
+                if matched:
+                    break
+            elif item["type"] == "regex":
+                result = re.match(item["value"], eventval)
+                if not None is result:
+                    matched = True
+                    break
+            else:
+                if item["value"] == eventval:
+                    matched = True
+                    break
+        if not matched:
+            # print "Selector "+str(selector_entry)+" did not match value "+eventval
+            return False
+        # print "Selector "+str(selector_entry)+" matched value "+eventval
+        return True
+
+    def blacklisted(self, event):
+        try:
+            for blitem in self.blacklist:
+                match = False
+                for selector in blitem["selectors"]:
+                    if not selector in event:
+                        break
+                    match = self.selectormatch(
+                        blitem["selectors"][selector], event[selector])
+                    if not match:
+                        break
+                if match:
+                    self.lh.info("Blacklist item matched:"+blitem["name"])
+                    if "severity" in blitem:
+                        event["SeverityName"] = blitem["severity"]
+                    return True
+            return False
+        except Exception as e:
+            self.lh.exception("Blacklist item match error:"+str(e))
+            return False
+        return False
+
+    def whitelisted(self, event):
+        try:
+            for wlitem in self.whitelist:
+                match = False
+                for selector in wlitem["selectors"]:
+                    if not selector in event:
+                        break
+                    match = self.selectormatch(
+                        wlitem["selectors"][selector], event[selector])
+                    if not match:
+                        break
+                if match:
+                    self.lh.info("Whitelist item matched:"+wlitem["name"])
+                    return True
+            return False
+        except Exception as e:
+            self.lh.exception("Whitelist item match error:"+str(e))
+            return False
+        return False
 
     def thehive_alert(self, alert):
         authheader = {'Content-Type': 'application/json',
                       'Authorization': 'Bearer '+self.conf['thehiveapi']}
         results = requests.post(self.conf['thehiveurl'], headers=authheader, data=json.dumps(
             alert), verify=self.conf['Verify_SSL'])
-        if results.status_code == 200:
+        if results.status_code > 199 and results.status_code < 300:
             self.lh.debug("Created alert:"+alert["title"])
         else:
-            self.lh.debug("Alert creation error:\n"+results.text)
+            self.lh.debug("Alert creation error:" +
+                          str(results.status_code)+"\n"+results.text)
 
     def caseTemplate(self, event):
         return self.conf['defaultCaseTemplate']
@@ -48,6 +212,8 @@ class TheHiveProcessor:
         return artifacts
 
     def processor(self, stream_data):
+        self.loadwhitelist(self.conf["whitelist_config"])
+        self.loadblacklist(self.conf["blacklist_config"])
         if stream_data['metadata']['eventType'] == 'DetectionSummaryEvent':
             event = stream_data['event']
             event_time = calendar.timegm(time.gmtime(
@@ -57,13 +223,25 @@ class TheHiveProcessor:
                 for tag in self.conf["tag_fields"]:
                     if tag in event:
                         tags.append(event[tag])
+
             if event_time > self.ts:
                 alert_message = ''
+
+                title = "[Falcon Host Detection - "+event["Technique"]+" via "+event["Tactic"] + \
+                    " on "+event["ComputerName"]+"/" + \
+                        event["UserName"]+"] "+event['DetectDescription']
+
+                if not self.blacklisted(event):
+                    if self.whitelisted(event):
+                        self.lh.info(
+                            "Whitelisted detection will be dropped:"+title)
+                        return
+                else:
+                    self.lh.info("Blacklisted event:"+title)
+
                 pretty_time = datetime.datetime.fromtimestamp(
                     time.time()).strftime('%Y-%m-%d %H:%M:%S')
-                title = "[Falcon Host Detection - "+event["Technique"]+" via "+event["Tactic"]+" on " + \
-                    event["ComputerName"]+"/"+event["UserName"]+"] " + \
-                        event['DetectDescription']  # +" ("+pretty_time+")"
+
                 sev = 3
                 if "medium" == event["SeverityName"].lower():
                     sev = 2
@@ -86,6 +264,7 @@ class TheHiveProcessor:
                 try:
                     self.thehive_alert(alert)
                     self.lh.info("Created new alert:"+alert['title'])
+                    self.email_alert(title, event)
                 except Exception as e:
                     self.lh.exception("Error while creating an alert"+str(e))
             else:
